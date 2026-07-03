7ff613cb705dcbe10c265b74d7c9701a50c6bd98
amol112225 <amolkendre18@gmail.com>
2026-07-03 09:26:50 +0530

    Time: 56 ms (12.66%), Space: 88.9 MB (7.5%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -8,11 +8,10 @@ public:
         while(r<n){
             mp[fruits[r]]++;
             if(mp.size()>2){
-                while(mp.size()>2){
+               
                     mp[fruits[l]]--;
                     if(mp[fruits[l]]==0) mp.erase(fruits[l]);
                     l++;
-                }
             }
             if(mp.size()<=2){
                     int len = r-l+1;
