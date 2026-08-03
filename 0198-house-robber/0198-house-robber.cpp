2c952d0bca824dd384ae7fab9df4a2a0697baca9
amol112225 <amolkendre18@gmail.com>
2026-08-03 19:56:32 +0530

    Time: 0 ms (100%), Space: 10.6 MB (64.98%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,11 +3,11 @@ public:
     int helper(int i, vector<int>&dp, vector<int>&nums){
         if(i>=nums.size()) return 0;
         if(dp[i]!=-1) return dp[i];
-        return dp[i] = nums[i] + max(helper(i+2,dp,nums),helper(i+3,dp,nums));
+        return dp[i] = max(nums[i] + helper(i+2,dp,nums),helper(i+1,dp,nums));
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int>dp(n+1,-1);
-        return max(helper(0,dp,nums),helper(1,dp,nums));
+        return helper(0,dp,nums);
     }
 };
