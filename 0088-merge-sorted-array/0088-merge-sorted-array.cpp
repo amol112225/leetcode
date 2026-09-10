468618165e8328d3e33c7bf127c9d3a97ae4f0d5
amol112225 <amolkendre18@gmail.com>
2026-09-10 13:33:21 +0530

    Time: 0 ms (100%), Space: 12.6 MB (20.56%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int>ans(nums1.begin(), nums1.end());
         int k = 0;
         for(int i=m; i<m+n; i++){
-            ans[i] = nums2[k];
+            nums1[i] = nums2[k];
             k++;
         }
-        sort(ans.begin(), ans.end());
-        nums1 = ans;
+        sort(nums1.begin(), nums1.end());
+
     }
 };
